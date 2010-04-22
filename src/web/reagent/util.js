2c5e83da50e5f721a36d544e2ae4ed598c3408dc
Adam Rauch <adam@labkey.com>
2010-04-22 03:04:20 +0000

    Update licenses
    SVN r13564 |2010-04-22 03:04:20 +0000

--- src/web/reagent/util.js
+++ src/web/reagent/util.js
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2009-2010 LabKey Corporation
+ *
+ * Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
+ */
 var ColumnSets = {
     Antigens: [ 'Name', 'Aliases', 'Description' ],
     Labels: [ 'Name', 'Aliases', 'Description' ],
